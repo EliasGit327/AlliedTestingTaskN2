428c6428ed8c23b351dd15b738e9a1aa03275cfc
Elias <gooodanonim69@gmail.com>
2020-10-22 13:40:23 +0300

    - Initial commit.

---
+++ AlliedTestingTaskN2.Application/Excel/Commands/CompareExcelFilesCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace AlliedTestingTaskN2.Application.Excel.Commands
+{
+    public class CompareExcelFilesCommand: IRequest<string>
+    {
+        public string FirstExcelFile { get; set; }
+        public string SecondExcelFile { get; set; }
+    }
+    
+    public class CompareExcelFilesCommandHandler: IRequestHandler<CompareExcelFilesCommand, string>
+    {
+        public async Task<string> Handle(CompareExcelFilesCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult($"{request.FirstExcelFile} - {request.SecondExcelFile}");
+        }
+    }
+}
---
+++ AlliedTestingTaskN2.Presenter/Controllers/ExcelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlliedTestingTaskN2.Application.Excel.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AlliedTestingTaskN2.Presenter.Controllers
+{
+    [ApiController]
+    [Route("excel")]
+    public class ExcelController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ExcelController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CompareExcelFiles([FromBody] CompareExcelFilesCommand request)
+        {
+            var result = await _mediator.Send(request);
+            return result != null ? Ok(result) : BadRequest() as ActionResult;
+        }
+    }
+}
